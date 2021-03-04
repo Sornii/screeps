01b8989f086eb42d691b8fbb594d16727feda7c2
Igor Fontana <igor193@gmail.com>
2021-03-03 23:48:41 -0300

    delivering resources, needs state machine

--- src/dice.js
+++ src/dice.js
@@ -1,3 +1,8 @@
+/**
+ * Zero-based index dice
+ * @param {number} sides The number of sides
+ * @returns {number} random side starting from zero
+ */
 export const roll = (sides) => {
-  return Math.floor(Math.random() * sides) + 1;
+  return Math.floor(Math.random() * sides);
 };
--- src/miner.js
+++ src/miner.js
@@ -1,6 +1,13 @@
-import { roll } from "./dice";
+import { roll } from './dice';
 
 const name = 'Miner';
+
+const STATES = {
+  MOVING_TO_MINE: 'movingToMine',
+  MOVING_TO_STORE: 'movingToStore',
+  STORING: 'storing',
+  MINING: 'mining',
+};
 
 /**
  * Give birth to a miner and put it to work
@@ -11,6 +18,10 @@ export const miner = (spawn) => {
 
   if (!miner) {
     spawn.spawnCreep([WORK, MOVE, CARRY], name);
+    return;
+  }
+
+  if (miner.spawning) {
     return;
   }
 
@@ -26,15 +37,69 @@ export const miner = (spawn) => {
     miner.memory.mine = mine.id;
   }
 
-  if (!miner.store.getFreeCapacity()) {
-    if (miner.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-      miner.moveTo(spawn);
+  if (!miner.memory.state) {
+    if (miner.store.getFreeCapacity() > 0) {
+      if (miner.pos.isNearTo(mine.pos)) {
+        miner.memory.state = STATES.MINING;
+      } else {
+        miner.memory.state = STATES.MOVING_TO_MINE;
+      }
+    } else {
+      if (miner.pos.isNearTo(spawn.pos)) {
+        miner.memory.state = STATES.STORING;
+      } else {
+        miner.memory.state = STATES.MOVING_TO_STORE;
+      }
     }
   }
 
-  if (mine) {
-    if (miner.harvest(mine) === ERR_NOT_IN_RANGE) {
+  // State-checker
+  switch (miner.memory.state) {
+    case STATES.MINING:
+      if (miner.store.getFreeCapacity() === 0) {
+        if (miner.pos.isNearTo(spawn.pos)) {
+          miner.memory.state = STATES.STORING;
+        } else {
+          miner.memory.state = STATES.MOVING_TO_STORE;
+        }
+      }
+      break;
+    case STATES.MOVING_TO_MINE:
+      if (miner.pos.isNearTo(mine.pos)) {
+        miner.memory.state = STATES.MINING;
+      } else {
+        miner.memory.state = STATES.MOVING_TO_MINE;
+      }
+      break;
+    case STATES.MOVING_TO_STORE:
+      if (miner.pos.isNearTo(spawn.pos)) {
+        miner.memory.state = STATES.STORING;
+      } else {
+        miner.memory.state = STATES.MOVING_TO_STORE;
+      }
+      break;
+    case STATES.STORING:
+      if (miner.store.getUsedCapacity() === 0) {
+        if (miner.pos.isNearTo(mine.pos)) {
+          miner.memory.state = STATES.MINING;
+        } else {
+          miner.memory.state = STATES.MOVING_TO_MINE;
+        }
+      }
+  }
+
+  // State-action
+  switch (miner.memory.state) {
+    case STATES.MINING:
+      miner.harvest(mine);
+      break;
+    case STATES.MOVING_TO_MINE:
       miner.moveTo(mine);
-    }
+      break;
+    case STATES.MOVING_TO_STORE:
+      miner.moveTo(spawn);
+      break;
+    case STATES.STORING:
+      miner.transfer(spawn, RESOURCE_ENERGY);
   }
 };
