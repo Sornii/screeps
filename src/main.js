324d8de99c3586a30abae019c4df366665baf3f3
Igor Fontana <igor193@gmail.com>
2021-03-04 15:58:54 -0300

    format

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { runMiner } from './runMiner';
-import { runBuilder } from "./runBuilder";
+import { runBuilder } from './runBuilder';
 
-const room = "E48N13";
+const room = 'E48N13';
 const spawn = 'Spawn1';
 
 // noinspection JSUnusedGlobalSymbols
@@ -11,18 +11,46 @@ export const loop = () => {
   );
   let spawn1 = Game.spawns[spawn];
 
-  const path = Game.rooms[room].findPath(spawn1.pos, Game.getObjectById('5bbcafe09099fc012e63b51e').pos);
-  const path2 = Game.rooms[room].findPath(spawn1.pos, Game.getObjectById('5bbcafe09099fc012e63b51f').pos);
-  const path3 = Game.rooms[room].findPath(spawn1.pos, new RoomPosition(17, 22, room));
-  const path4 = Game.rooms[room].findPath(spawn1.pos, new RoomPosition(21, 23, room));
-  Game.rooms[room].visual.poly(path, {stroke: '#fff', strokeWidth: .15,
-    opacity: .2, lineStyle: 'dashed'});
-  Game.rooms[room].visual.poly(path2, {stroke: '#fff', strokeWidth: .15,
-    opacity: .2, lineStyle: 'dashed'});
-  Game.rooms[room].visual.poly(path3, {stroke: '#fff', strokeWidth: .15,
-    opacity: .2, lineStyle: 'dashed'});
-  Game.rooms[room].visual.poly(path4, {stroke: '#fff', strokeWidth: .15,
-    opacity: .2, lineStyle: 'dashed'});
+  const path = Game.rooms[room].findPath(
+    spawn1.pos,
+    Game.getObjectById('5bbcafe09099fc012e63b51e').pos
+  );
+  const path2 = Game.rooms[room].findPath(
+    spawn1.pos,
+    Game.getObjectById('5bbcafe09099fc012e63b51f').pos
+  );
+  const path3 = Game.rooms[room].findPath(
+    spawn1.pos,
+    new RoomPosition(17, 22, room)
+  );
+  const path4 = Game.rooms[room].findPath(
+    spawn1.pos,
+    new RoomPosition(21, 23, room)
+  );
+  Game.rooms[room].visual.poly(path, {
+    stroke: '#fff',
+    strokeWidth: 0.15,
+    opacity: 0.2,
+    lineStyle: 'dashed',
+  });
+  Game.rooms[room].visual.poly(path2, {
+    stroke: '#fff',
+    strokeWidth: 0.15,
+    opacity: 0.2,
+    lineStyle: 'dashed',
+  });
+  Game.rooms[room].visual.poly(path3, {
+    stroke: '#fff',
+    strokeWidth: 0.15,
+    opacity: 0.2,
+    lineStyle: 'dashed',
+  });
+  Game.rooms[room].visual.poly(path4, {
+    stroke: '#fff',
+    strokeWidth: 0.15,
+    opacity: 0.2,
+    lineStyle: 'dashed',
+  });
 
   const constructionSites = spawn1.room.find(FIND_CONSTRUCTION_SITES);
   runBuilder(spawn1, constructionSites);
