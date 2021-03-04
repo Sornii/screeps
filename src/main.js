03e0e35f85ff21f6440d96c5ccfdce56e6fec0c7
Igor Fontana <igor193@gmail.com>
2021-03-04 11:14:47 -0300

    miners last

--- src/main.js
+++ src/main.js
@@ -9,14 +9,15 @@ export const loop = () => {
     `Running, there's ${Game.cpu.tickLimit} of CPU to spend on this tick.`
   );
   let spawn1 = Game.spawns[spawn];
+
+  const constructionSites = spawn1.room.find(FIND_CONSTRUCTION_SITES);
+  runBuilder(spawn1, constructionSites);
+  runBuilder(spawn1, constructionSites, 'II');
+
   runMiner(spawn1);
   runMiner(spawn1, 'II');
   runMiner(spawn1, 'III');
   runMiner(spawn1, 'IV');
   runMiner(spawn1, 'V');
   runMiner(spawn1, 'VI');
-
-  const constructionSites = spawn1.room.find(FIND_CONSTRUCTION_SITES);
-  runBuilder(spawn1, constructionSites);
-  runBuilder(spawn1, constructionSites, 'II');
 };
