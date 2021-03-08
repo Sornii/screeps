c4f944c9977972fe85a272fb666afe10e4e5b0e1
Igor Fontana <igor193@gmail.com>
2021-03-08 20:26:25 -0300

    checks for viewer

--- src/viewer.js
+++ src/viewer.js
@@ -1,12 +1,15 @@
-import { each } from "lodash";
+import { each } from 'lodash';
 
 export const viewer = (worldState) => {
-
   const { roads } = worldState;
 
-  each(roads, road => {
+  each(roads, (road) => {
     const { roomId, fromX, fromY, toX, toY } = road;
     const room = Game.rooms[roomId];
+
+    if (!room) {
+      console.log(`Can't create visual for path. Room doesn't exist.`);
+    }
 
     const from = room.getPositionAt(fromX, fromY);
     const to = room.getPositionAt(toX, toY);
