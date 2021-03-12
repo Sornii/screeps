c3e82fec0e7e757ef00a2fdbc4576b31d0ec4c64
Igor Fontana <igor193@gmail.com>
2021-03-12 00:49:33 -0300

    room extractor

---
+++ src/roomExtractor.js
@@ -0,0 +1,7 @@
+export const roomExtractor = () => {
+  console.log('----==== Room extractor ===---');
+
+  console.log('----==== Room extractor ===---');
+
+  console.log('----==== Room extractor ===---');
+};
