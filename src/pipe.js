563443b8a4b045e783e49f34efe302a96a69d28c
Igor Fontana <igor193@gmail.com>
2021-03-13 12:03:26 -0300

    timed pipe

--- src/pipe.js
+++ src/pipe.js
@@ -3,18 +3,16 @@ export const pipe = (...fns) => (...args) =>
 
 export const timedPipe = (timer) => (...fns) => (...args) =>
   pipe(
-    fns.map(
-      (fn) => (...args) => {
-        const start = timer.start();
-        const result = fn(...args);
-        const end = timer.end();
-        const time = timer.calc(start, end);
-        const formatted = timer.format(time);
+    fns.map((fn) => (...args) => {
+      const start = timer.start();
+      const result = fn(...args);
+      const end = timer.end();
+      const time = timer.calc(start, end);
+      const formatted = timer.format(time);
 
-        console.log(`Spent ${formatted} on ${fn.name}`);
+      console.log(`Spent ${formatted} on ${fn.name}`);
 
-        return result;
-      },
-      ...args
-    )
+      return result;
+    }),
+    ...args
   );
